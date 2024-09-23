bef0b03aafb2127720c2bc00e69ff4232025d760
joosomi <wnthal1211@gmail.com>
2024-09-23 20:30:35 +0900

    🔨 [Chore] Swagger 설정 (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 import { AppModule } from './app.module';
@@ -7,6 +8,17 @@ import { GlobalExceptionsFilter } from './filter/global-exception.filter';
 const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Bookpecker API')
+    .setDescription('Bookpecker 서비스의 API의 명세서입니다.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useGlobalFilters(new GlobalExceptionsFilter(logger));
