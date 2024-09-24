5b8bd3217455b9176674703e49c8a41d1113d99d
joosomi <wnthal1211@gmail.com>
2024-09-24 20:02:30 +0900

    ✨ [Feature] OAuth 컨트롤러, 서비스, 모듈 생성 (#5)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { WinstonModule } from 'nest-winston';
 
+import { OauthController } from './feature/rest/oauth/oauth.controller';
+import { OAuthModule } from './feature/rest/oauth/oauth.module';
 import { winstonOptions } from './logging/logger';
 import { PrismaModule } from './prisma/prisma.module';
 
@@ -12,8 +14,9 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true, // 전역 설정 - 모든 모듈에서 ConfigService 사용 가능
     }),
     PrismaModule,
+    OAuthModule,
   ],
-  controllers: [],
+  controllers: [OauthController],
   providers: [],
 })
 export class AppModule {}
